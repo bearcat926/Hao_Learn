3686956bd86da66506854a2f2ab96b4a720bb6df
bearcat926 <hh18622073649@gmail.com>
2019-09-06 18:40:54 +0800

    2019年9月6日18:40:53

--- 2019/9/offer/Solution6.java
+++ 2019/9/offer/Solution6.java
@@ -1,27 +1,19 @@
-import java.util.Arrays;
-
 /**
- * 从尾到头打印链表 输入一个链表的头结点，按照 从尾到头 的顺序返回节点的值。 返回的结果用数组存储。
+ * 从尾到头打印链表 
+ * 输入一个链表的头结点，按照 从尾到头 的顺序返回节点的值。 
+ * 返回的结果用数组存储。
  */
 
 // Definition for singly-linked list.
 class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) { val = x; }
-}
-/**
-     * ListNode i, j, temp = head;
-        
-        while(temp.next != null){
-            i = temp.next;
-            j = i.next;
-            i.next = temp;
-            temp = j;
-        }
 
-        head.next = null;
-     */
+    ListNode(int x) {
+        val = x;
+    }
+}
+
 class Solution6 {
     public static int[] printListReversingly(ListNode head) {
         ListNode temp = head;
@@ -34,13 +26,23 @@ class Solution6 {
         int i = length - 1;
         int[] array = new int[length];
         // 数组赋值
-        while (head != null){
+        while (head != null) {
             array[i--] = head.val;
             head = head.next;
         }
         return array;
     }
-    
+
+    /**
+     * 链表反转：需要前中后三个临时节点，先存储后节点，在对当前节点的下一节点赋值为前节点，之后将前中节点依次置后
+     * while (curr.next != null) {
+            next = curr.next;
+            curr.next = pre; 
+            pre = curr; 
+            curr = next; 
+        }
+     * 
+     */
     public static void main(String[] args) {
         ListNode head = new ListNode(2);
         head.next = new ListNode(3);
