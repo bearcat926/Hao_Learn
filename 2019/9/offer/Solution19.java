68f4f8137edc429360bacb7f9e6a9926f8bcb16f
bearcat926 <43287323+bearcat926@users.noreply.github.com>
2019-09-13 23:20:08 +0800

    Update Solution19.java

--- 2019/9/offer/Solution19.java
+++ 2019/9/offer/Solution19.java
@@ -6,16 +6,48 @@
  * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配。
  */
 class Solution19 {
-    public static boolean isMatch(String s, String p) {
-        char[] dest = s.toCharArray();
-        char[] mode = p.toCharArray();
 
-        for (int i = 0; i < mode.length; i++) {
-
-        }
-
-        
+    int sLength, pLength;
+    char[] sc, pc;
+    boolean[][] f ;
+	
+    public boolean isMatch(String s, String p) {
+	// 非空检测
+	if (s == null || p == null) {
+	    return false;
+	}
+	
+	sc = s.toCharArray();
+	pc = p.toCharArray();
+	sLength = sc.length;
+	pLength = pc.length;
+	
+	// pLength + 1 为了防止 y + 1 时出现空指针的情况
+	f = new boolean[sLength][pLength + 1];
+	return matchCoreByDP(0, 0);  
     }
+
+    public boolean matchCoreByDP(int x, int y) {
+        // 两个长度都相同则返回；也可以防止 `.` or `.*` 模式对空字符串的不匹配情况
+	if (y == pLength) {
+	    return x == sLength;
+	}      
+	
+	// 对当前需要检测的字符进行判断，分为 字符是否相等 和 当前的匹配模式是否是 `.`
+	boolean currMatch = x < sLength && (sc[x] == pc[y] || pc[y] == '.');
+
+	// 判断下一个匹配模式是否是 '*'
+	if (y + 1 < pLength && pc[y + 1] == '*') {
+	    // 判断 0次 和 1次 两种情况。其中，判断 1次 时，要检测对当前字符的判断结果
+	    return matchCoreByDP(x, y + 2) || currMatch && matchCoreByDP(x + 1, y);
+	} else {
+	    // 检测的对第一个字符的判断结果.通过则继续下一字符的判断
+	    return currMatch  && matchCoreByDP(x + 1, y + 1);
+	}
+
+    }
+
+
 
     public static void main(String[] args) {
         String s = "aa";
