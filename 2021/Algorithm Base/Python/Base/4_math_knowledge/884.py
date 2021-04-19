84a673327939cb3bcc900bad12c4ecb4a47dcece
bearcat926 <hh18622073649@gmail.com>
2021-04-20 00:29:55 +0800

    2021年4月20日00:29:49

--- 2021/Algorithm Base/Python/Base/4_math_knowledge/884.py
+++ 2021/Algorithm Base/Python/Base/4_math_knowledge/884.py
@@ -37,4 +37,68 @@ M[n][1]x[1] ^ M[n][2]x[2] ^ … ^ M[n][n]x[n] = B[n]
 1
 0
 0
+=============================================
+异或 => 不进位加法
+
+    对结果的判断：
+        1. 完美阶梯型 - 唯一解
+        2. 有矛盾    - 无解
+        3. 无矛盾    - 无穷多组解
+
+    高斯消元的具体步骤：
+
+        枚举每一列c
+            1. 枚举列
+            2. 非零行
+            3. 交换
+            4. 下面清零
+
+
+
 """
+S = []
+n = 0
+
+
+def gaussian_elimination():
+    global S
+    r = 0
+    for c in range(n):  # 枚举列
+        t = r
+        # 找到非零的一行
+        for i in range(r, n):
+            if S[i][c] != 0:
+                t = i
+                break
+        if S[t][c] == 0:  # 该列全为0
+            continue
+        # 交换两列
+        S[t], S[r] = S[r], S[t]
+        # 下面清零
+        for k in range(r + 1, n):
+            if S[k][c] != 0:
+                S[k] = [S[k][x] for x in range(c)] + [S[k][x] ^ S[r][x] for x in range(c, n + 1)]
+        r += 1  # 最上面的行下降一层
+
+    if r < n:  # 消元过程中跳行了
+        for i in range(r, n):
+            if round(S[i][n]) != 0:  # 末尾不等于0
+                print('No solution')  # 无解
+                return
+        print('Multiple sets of solutions')  # 无穷多组解
+        return
+
+    for c in range(n - 1, -1, -1):  # 处理第 c列
+        for k in range(0, c):  # 消除上面行
+            if S[k][c] != 0:
+                S[k] = [S[k][x] for x in range(c)] + [S[k][x] ^ S[c][x] for x in range(c, n + 1)]
+
+    for i in range(n):
+        print(S[i][n])
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        S.append(list(map(int, input().split())))
+    gaussian_elimination()
